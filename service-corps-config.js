c87c25e833019dd70c5f55946c9979fdd37f15c3
MultigrainIntl <george@multigrain.com>
2026-07-03 14:43:45 -0600

    Add safe Service Corps endpoint configuration

---
+++ service-corps-config.js
@@ -0,0 +1,13 @@
+/*
+ * Food Aid Project Service Corps public configuration.
+ *
+ * Leave endpoint empty until the reviewed Google Apps Script web app is deployed.
+ * Never place a spreadsheet ID, API key, token, respondent data, or other secret here.
+ */
+window.FAP_SERVICE_CORPS_CONFIG = Object.freeze({
+  endpoint: '',
+  responseOrigins: [
+    'https://script.google.com',
+    'https://script.googleusercontent.com'
+  ]
+});
